50b5b5752e88bd878322cf4fcef2fc18698d62bc
Anca-Nicoleta Ciubotaru <aci@zurich.ibm.com>
2018-05-24 14:47:58 +0200

    Update Story class to support wrong and right andwers

--- story.py
+++ story.py
@@ -1,12 +1,15 @@
 class Story():
-    def __init__(self, id, name, s1, s2, s3, s4, e_correct):
+    def __init__(self, id, s1, s2, s3, s4, e_right, e_wrong = ""):
         self.id = id
-        self.name = name
         self.s1 = s1
         self.s2 = s2
         self.s3 = s3
         self.s4 = s4
-        self.e_correct = e_correct
+        self.e_right = e_right
+        self.e_wrong = e_wrong
 
-    def get_story_as_list(self):
-        return [self.s1, self.s2, self.s3, self.s4, self.e_correct]
+    def get_story_with_right_ending_as_list(self):
+        return [self.s1, self.s2, self.s3, self.s4, self.e_right]
+
+    def get_story_with_both_endings_as_list(self):
+        return [self.s1, self.s2, self.s3, self.s4, self.e_right, self.e_wrong]
