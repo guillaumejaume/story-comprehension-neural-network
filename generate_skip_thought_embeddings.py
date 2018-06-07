56b289d090eaeda20c941c65a6610b660c3dbd0c
Anca-Nicoleta Ciubotaru <aci@zurich.ibm.com>
2018-06-07 15:07:32 +0200

    Update file generate_skip_thought_embeddings for embedding generation

--- generate_skip_thought_embeddings.py
+++ generate_skip_thought_embeddings.py
@@ -1,10 +1,73 @@
 import numpy as np
 import skipthoughts
-import utils
 from story import Story
 import struct
+import csv
 
-def generate_and_save_word_embeddings_for_sentences_text(input_file, embeddings_output_path, embeddings_id_output_file, for_testing = False):
+
+def load_and_process_text_data(filename, for_testing=False, is_labeled=True):
+    """ Load a file and create a list of sentences
+    Parameters:
+    -----------
+    filename: string
+    path to the file to load
+
+    Returns:
+    --------
+    raw: list of sentences
+    """
+    list_of_sentences = []
+    first_line = True
+
+    with open(filename) as f:
+        reader = csv.reader(f)
+        story_id = 0
+        for row in reader:
+            if first_line and row[0] == "InputStoryId":
+                first_line = False
+            else:
+                if not for_testing:
+                    id = row[0]
+                    s1 = row[2]
+                    s2 = row[3]
+                    s3 = row[4]
+                    s4 = row[5]
+                    re = row[6]
+                    list_of_sentences.append(Story(id, s1, s2, s3, s4, re))
+
+                else:
+                    if is_labeled:
+                        if int(row[7]) == 1:
+                            id = row[0]
+                            s1 = row[1]
+                            s2 = row[2]
+                            s3 = row[3]
+                            s4 = row[4]
+                            re = row[5]
+                            we = row[6]
+                        else:
+                            id = row[0]
+                            s1 = row[1]
+                            s2 = row[2]
+                            s3 = row[3]
+                            s4 = row[4]
+                            re = row[6]
+                            we = row[5]
+                    else:
+                        id = str(story_id)
+                        s1 = row[0]
+                        s2 = row[1]
+                        s3 = row[2]
+                        s4 = row[3]
+                        re = row[4]
+                        we = row[5]
+                        story_id = story_id + 1
+
+                    list_of_sentences.append(Story(id, s1, s2, s3, s4, re, we))
+
+    return list_of_sentences
+
+def generate_and_save_word_embeddings_for_sentences_text(input_file, embeddings_output_path, embeddings_id_output_file, for_testing = False, is_labeled = True):
     """ Generate the embeddings and save them in a different file
        Parameters:
        -----------
@@ -17,13 +80,13 @@ def generate_and_save_word_embeddings_for_sentences_text(input_file, embeddings_
        embeddings_id_output_file: string
        path to the file to save the embeddings id in
     """
-    list_of_stories = utils.load_and_process_text_data(input_file, for_testing)
+    list_of_stories = load_and_process_text_data(input_file, for_testing, is_labeled)
     model = skipthoughts.load_model()
     encoder = skipthoughts.Encoder(model)
 
-    print("len(list_of_stories) ",len(list_of_stories))
     fout_id = open(embeddings_id_output_file, "wb")
     for story in list_of_stories:
+        print(story)
         if not for_testing:
             embeddings = encoder.encode(story.get_story_with_right_ending_as_list())
         else:
@@ -40,5 +103,5 @@ def generate_and_save_word_embeddings_for_sentences_text(input_file, embeddings_
     fout_id.close()
 
 
-#generate_and_save_word_embeddings_for_sentences_text("./data/train_stories.csv","./data/embeddings/", "./data/embeddings/id.txt")
-generate_and_save_word_embeddings_for_sentences_text("./data/validation.csv","./data/embeddings_test/", "./data/embeddings_test/id.txt", True)
+generate_and_save_word_embeddings_for_sentences_text("./data/test_nlu18.csv","./data/embeddings_test_eth_test/", "./data/embeddings_test_eth_test/id.txt", True, False)
+
